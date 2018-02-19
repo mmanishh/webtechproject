564bba9632f0f06463831edc0d6ca45d0ae39bc0
mmanishh <dfrozenthrone@gmail.com>
2018-02-19 23:10:37 +0545

    dynamic nav load

--- header.php
+++ header.php
@@ -34,13 +34,52 @@ error_reporting(E_ALL); // report all php errors
     <body>
 
         <!-- nav -->
+        <?php 
 
-        <nav class="navbar navbar-inverse">
+            include_once 'initial.php';
+            include_once 'classes/pages.php';
+
+
+            $pages = new Pages($db);
+
+            // select all users
+            $prep_state = $pages->getAll();
+            $num = $prep_state->rowCount();
+
+
+            if($num>=0){
+
+                echo "<nav class=\"navbar navbar-inverse\">";
+                echo "<div class=\"container-fluid\">";
+                echo "<div class=\"navbar-header\">";
+                echo "<a class=\"navbar-brand\" href=\"#\">Students Hub</a>";
+                echo "</div>";
+                echo "<ul class=\"nav navbar-nav\">";
+
+                while ($row = $prep_state->fetch(PDO::FETCH_ASSOC)){
+
+                    extract($row); //Import variables into the current symbol table from an array
+
+                    echo "<li class=\"active\"><a href=\"$row[page_url]\">$row[title]</a></li>";
+
+        
+                }
+                //end for ul tags for pages
+                echo "</ul>";
+
+            }
+
+            // if there are no pages
+            else{
+                echo "<div> No Page found. </div>";
+                }
+        ?>
+        <!-- <nav class="navbar navbar-inverse">
             <div class="container-fluid">
                 <div class="navbar-header">
-                    <a class="navbar-brand" href="#">WebSiteName</a>
+                    <a class="navbar-brand" href="#">Students Hub</a>
                 </div>
-                <ul class="nav navbar-nav">
+                <ul class="nav navbar-nav"> 
                     <li class="active"><a href="#">Home</a></li>
                     <li><a href="#">Page 1</a></li>
                     <li><a href="#">Page 2</a></li>
@@ -48,7 +87,7 @@ error_reporting(E_ALL); // report all php errors
                 <ul class="nav navbar-nav navbar-right">
                     <li><a href="#"><span class="glyphicon glyphicon-user"></span> Sign Up</a></li>
                     <li><a href="#"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
-                </ul>
+                </ul> -->
             </div>
         </nav>
 

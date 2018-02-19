133b36092d4d05a019cf88f6d30665e5fa3e066f
mmanishh <dfrozenthrone@gmail.com>
2018-02-19 23:11:30 +0545

    added about.php

---
+++ about.php
@@ -0,0 +1,25 @@
+<?php 
+
+// include database and object files
+include_once 'classes/database.php';
+include_once 'classes/user.php';
+include_once 'classes/category.php';
+include_once 'initial.php';
+
+
+$page_title = "About";
+
+include_once 'header.php';
+
+echo "<div class=\"container\">";
+echo "<div class=\"jumbotron\">";
+echo "<h1>Web Tech Project</h1> ";
+echo "<p>This is web tech Project assignment </p>";
+echo "</div>";
+echo "</div>";
+    
+ 
+
+include_once 'footer.php';
+
+ ?>

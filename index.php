e074166c851cfd5c450b32ca38daf8333116d530
mmanishh <dfrozenthrone@gmail.com>
2018-03-05 22:28:50 +0545

    changes in redirection

--- index.php
+++ index.php
@@ -20,19 +20,16 @@ $category = new Category($db);
 		WLogin
 	</div>
 	<?php
-	if(isset($_SESSION['valid'])) {			
-		//$result = mysqli_query($mysqli, "SELECT * FROM login");
+	if(isset($_SESSION['valid']))
+    {			
+            header('Location: home.php');  
+    }else{
+            header('Location: login.php');   
+
+    } 
 	?>
 				
-		Welcome <?php echo $_SESSION['name'] ?> ! <a href='logout.php'>Logout</a><br/>
-		<br/>
-		<br/><br/>
-	<?php	
-	} else {
-		echo "You must be logged in to view this page.<br/><br/>";
-		echo "<a href='login.php'>Login</a> | <a href='register.php'>Register</a>";
-	}
-	?>
+	
 	
 <?php
 include_once "footer.php";
